1ac4b5f09ec142793daf06fd5d7ff289292e14b7
Freddie <421444710@qq.com>
2017-11-26 21:49:14 +0800

    Initial commit

---
+++ src/test/java/com/myzhihu/MyzhihuApplicationTests.java
@@ -0,0 +1,16 @@
+package com.myzhihu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyzhihuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
